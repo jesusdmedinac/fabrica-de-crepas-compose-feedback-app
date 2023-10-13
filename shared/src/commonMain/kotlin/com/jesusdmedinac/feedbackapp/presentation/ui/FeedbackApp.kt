e13282e5374e34e033b0a445e8aaed03f0c561da
Jesús Daniel Medina Cruz <jesus@beek.io>
2023-10-12 23:43:21 -0700

    Remove unused imports

--- shared/src/commonMain/kotlin/com/jesusdmedinac/feedbackapp/presentation/ui/FeedbackApp.kt
+++ shared/src/commonMain/kotlin/com/jesusdmedinac/feedbackapp/presentation/ui/FeedbackApp.kt
@@ -2,11 +2,8 @@ package com.jesusdmedinac.feedbackapp.presentation.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
